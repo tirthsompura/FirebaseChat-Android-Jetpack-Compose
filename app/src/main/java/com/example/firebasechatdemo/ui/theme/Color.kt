e94746187ed69ef5a256529da23f42a3c1fc9dbf
mqa-2 <multiqostech@gmail.com>
2024-05-24 13:24:38 +0530

    [-] First Commit

--- app/src/main/java/com/example/firebasechatdemo/ui/theme/Color.kt
+++ app/src/main/java/com/example/firebasechatdemo/ui/theme/Color.kt
@@ -10,8 +10,6 @@ val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
-val disabledTextFieldColor = Color(0xFFB6B6B6)
-
 val whiteColor = Color(0xFFFAFAFA)
 val blueBgColor = Color(0xFF292F3F)
 val blueBgColorLight = Color(0xFF404961)
